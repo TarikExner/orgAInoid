0e6476cd88277fca262daf01565139d38c83e462
TarikExner <Tarik.Exner@gmail.com>
2024-08-22 19:42:33 +0200

    fixed bug in MLP

--- orgAInoid/rpe_classification/models.py
+++ orgAInoid/rpe_classification/models.py
@@ -988,7 +988,7 @@ class MLP_FC3(nn.Module):
         
     def forward(self, x):
         # Flatten the image
-        x = x.view(x.size, -1)
+        x = x.view(x.size(0), -1)
         
         # Fully connected layers with ReLU activations
         x = F.relu(self.fc1(x))
@@ -1018,7 +1018,7 @@ class MLP(nn.Module):
         
     def forward(self, x):
         # Flatten the image
-        x = x.view(x.size, -1)
+        x = x.view(x.size(0), -1)
         
         # Fully connected layers with ReLU activations
         x = F.relu(self.fc1(x))
