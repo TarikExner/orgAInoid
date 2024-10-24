d689ed6bb2b5fb3520747815f26ddb1aec12a381
TarikExner <Tarik.Exner@gmail.com>
2024-10-24 12:12:29 +0200

    debug print

--- orgAInoid/_augmentation.py
+++ orgAInoid/_augmentation.py
@@ -47,6 +47,7 @@ class NormalizeSegmented(DualTransform):
 
     def apply(self, img, **params):
         mask = params['mask']
+        print(mask.shape)
 
         # Ensure the mask is correctly shaped to match the image dimensions
         mask = mask.astype(bool)
@@ -67,7 +68,6 @@ class NormalizeSegmented(DualTransform):
 
         # Identify new zero-pixels introduced by augmentations (which were not part of the original mask)
         new_zero_pixels = (img == 0) & (~mask)
-        print(new_zero_pixels.shape)
 
         # Set these newly introduced zero-pixels to the calculated fill_value
         img_normalized[new_zero_pixels] = fill_value
