5881a11dc561a620381c70d07baf20729571e02c
TarikExner <Tarik.Exner@gmail.com>
2024-10-24 12:11:48 +0200

    fixed mask bug

--- orgAInoid/_augmentation.py
+++ orgAInoid/_augmentation.py
@@ -67,6 +67,7 @@ class NormalizeSegmented(DualTransform):
 
         # Identify new zero-pixels introduced by augmentations (which were not part of the original mask)
         new_zero_pixels = (img == 0) & (~mask)
+        print(new_zero_pixels.shape)
 
         # Set these newly introduced zero-pixels to the calculated fill_value
         img_normalized[new_zero_pixels] = fill_value
