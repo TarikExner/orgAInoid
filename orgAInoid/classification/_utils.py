488a589f5f4e302ca4a6aa2f598b46cef9630bdc
TarikExner <Tarik.Exner@gmail.com>
2024-11-28 10:35:49 +0100

    old new normalization

--- orgAInoid/classification/_utils.py
+++ orgAInoid/classification/_utils.py
@@ -409,8 +409,8 @@ class AugmentationScheduler:
         }
 
     def _normalization(self):
-        #return NormalizeSegmented(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        return A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value = 1)
+        return NormalizeSegmented(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        # return A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value = 1)
 
 
     def _basic_augmentations(self):
