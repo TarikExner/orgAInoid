98b227608069e42ae745bc3abf710c85d1f6dada
TarikExner <Tarik.Exner@gmail.com>
2024-12-02 21:32:45 +0100

    lowered lr for model temp scaling

--- orgAInoid/classification/_evaluation.py
+++ orgAInoid/classification/_evaluation.py
@@ -88,7 +88,7 @@ class ModelWithTemperature(nn.Module):
         print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
 
         # Next: optimize the temperature w.r.t. NLL
-        optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=10_000)
+        optimizer = optim.LBFGS([self.temperature], lr=0.001, max_iter=10_000)
 
         def eval():
             optimizer.zero_grad()
