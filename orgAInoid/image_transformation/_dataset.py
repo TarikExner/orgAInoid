0f8e86b0e5a12ff1b5bd7a0acbd693ff684245cb
TarikExner <Tarik.Exner@gmail.com>
2025-01-11 09:35:56 +0100

    indices to int

--- orgAInoid/image_transformation/_dataset.py
+++ orgAInoid/image_transformation/_dataset.py
@@ -131,7 +131,7 @@ class ImageSequenceDataset(Dataset):
                 self.sequence_indices.append((input_indices, target_indices))
         
         # Convert list to NumPy array for faster indexing
-        self.sequence_indices = np.array(self.sequence_indices, dtype=object)
+        self.sequence_indices = np.array(self.sequence_indices, dtype=np.int64)
         print(f"Total sequences available with gap={self.gap}: {len(self.sequence_indices)}")
     
     def __len__(self):
