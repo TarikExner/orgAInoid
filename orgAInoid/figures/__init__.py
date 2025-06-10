535e9b0f265c3a8484bfc4168ef320e8bdfbcaf7
TarikExner <Tarik.Exner@gmail.com>
2025-06-10 14:14:23 +0200

    init files

---
+++ orgAInoid/figures/__init__.py
@@ -0,0 +1,15 @@
+from figure_1 import figure_1_generation
+from figure_2 import figure_2_generation
+
+from figure_S1 import figure_S1_generation
+from figure_S2 import figure_S2_generation
+
+
+
+__all__ = [
+    "figure_1_generation",
+    "figure_2_generation",
+
+    "figure_S1_generation",
+    "figure_S2_generation"
+]
