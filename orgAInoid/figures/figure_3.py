b8023b57b1a45fb47fc20a406148a8f2b842b82a
TarikExner <Tarik.Exner@gmail.com>
2025-06-24 12:10:11 +0200

    just seeing

--- orgAInoid/figures/figure_3.py
+++ orgAInoid/figures/figure_3.py
@@ -81,7 +81,7 @@ def _generate_main_figure(rpe_f1: pd.DataFrame,
 
         
         handles, labels = accuracy_plot.get_legend_handles_labels()
-        labels = ["CNN (image data): Validation", "CNN (image data): Test", "Random Forest (morphometrics): Validation", "Random Forest (morphometrics): Test", "Expert prediction"]
+        # labels = ["CNN (image data): Validation", "CNN (image data): Test", "Random Forest (morphometrics): Validation", "Random Forest (morphometrics): Test", "Expert prediction"]
         accuracy_plot.legend(handles, labels, loc = "lower right", fontsize = cfg.TITLE_SIZE)
         accuracy_plot.set_title("Prediction accuracy: Emergence of RPE", fontsize = cfg.TITLE_SIZE)
         accuracy_plot.set_ylabel("F1 score", fontsize = cfg.AXIS_LABEL_SIZE)
@@ -131,7 +131,7 @@ def _generate_main_figure(rpe_f1: pd.DataFrame,
         )
 
         handles, labels = accuracy_plot.get_legend_handles_labels()
-        labels = ["CNN (image data): Validation", "CNN (image data): Test", "QDA (morphometrics): Validation", "QDA (morphometrics): Test", "Expert prediction"]
+        # labels = ["CNN (image data): Validation", "CNN (image data): Test", "QDA (morphometrics): Validation", "QDA (morphometrics): Test", "Expert prediction"]
         accuracy_plot.legend(handles, labels, loc = "lower right", fontsize = cfg.TITLE_SIZE)
         accuracy_plot.set_title("Prediction accuracy: Emergence of Lenses", fontsize = cfg.TITLE_SIZE)
         accuracy_plot.set_ylabel("F1 score", fontsize = cfg.AXIS_LABEL_SIZE)
